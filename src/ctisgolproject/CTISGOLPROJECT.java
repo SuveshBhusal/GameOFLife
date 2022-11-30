90bfd1f11a8021c99c311dbf1352748eddf5ada8
SuveshBhusal <suvsal8@gmail.com>
2022-11-29 21:06:27 -0500

    test211/29/22

--- src/ctisgolproject/CTISGOLPROJECT.java
+++ src/ctisgolproject/CTISGOLPROJECT.java
@@ -16,6 +16,7 @@ public class CTISGOLPROJECT {
     public static void main(String[] args) {
         // TODO code application logic here
         //lets see if it worked
+        //test2
     }
     
 }
