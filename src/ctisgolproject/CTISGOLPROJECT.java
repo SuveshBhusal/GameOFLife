240efea26fd9c6a64462bbcf03e750fb4f30afbb
TheFluffyNebula <yinj@guilford.edu>
2022-11-29 21:30:49 -0500

    edited driver program

--- src/ctisgolproject/CTISGOLPROJECT.java
+++ src/ctisgolproject/CTISGOLPROJECT.java
@@ -1,12 +1,43 @@
 package ctisgolproject;
 
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+
 public class CTISGOLPROJECT {
 
+    /*default parameters:
+    grid = 10x10
+    intial alive = 40% 
+    */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //lets see if it worked
-        //test2
-        //test3
+        //GOLGUI theGUI = new GOLGUI();
+        Block theBlock = new Block();
+        ArrayList<ArrayList<Block>> theGrid = theBlock.initGrid(10); //grid setup
+        int generation = 0;
+        JFrame startWindow = new JFrame("Jason and Eric's Conway's Game Of Life");
+        startWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //startWindow.add(theGUI);
+        startWindow.pack();
+        startWindow.setVisible(true);   
+        //start the simulation
+        for (int i=0;i<20;i++){
+            //methods: update neighbors alive
+            theBlock.updateNeighborsAlive(theGrid, 10);
+            //update alive
+            theBlock.updateAlive(theGrid,10);
+            generation+=1;
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(CTISGOLPROJECT.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        System.out.println(generation);
     }
+    /*
+    parameterize initGrid, checkNeighbors
+    */
     
 }
