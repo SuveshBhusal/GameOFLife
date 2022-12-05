3fa412d151506f5f62983c108868a71629b0e4fb
TheFluffyNebula <yinj@guilford.edu>
2022-12-04 20:59:09 -0500

    init draw1()

--- src/ctisgolproject/CTISGOLPROJECT.java
+++ src/ctisgolproject/CTISGOLPROJECT.java
@@ -1,9 +1,5 @@
 package ctisgolproject;
 
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class CTISGOLPROJECT {
 
     /*default parameters:
@@ -12,28 +8,12 @@ public class CTISGOLPROJECT {
     */
     public static void main(String[] args) {
         GOLGUI theGUI = new GOLGUI();
-        Block theBlock = new Block();
-        ArrayList<ArrayList<Block>> theGrid = theBlock.initGrid(10); //grid setup
-        int generation = 0;
         //JFrame startWindow = new JFrame("Jason and Suvesh's Conway's Game Of Life");
         //startWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //startWindow.getContentPane().add(theGUI);
         //startWindow.pack();
         theGUI.setVisible(true);   
         //start the simulation
-        for (int i=0;i<20;i++){
-            //methods: update neighbors alive
-            theBlock.updateNeighborsAlive(theGrid, 10);
-            //update alive
-            theBlock.updateAlive(theGrid,10);
-            generation+=1;
-            try {
-                Thread.sleep(20);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(CTISGOLPROJECT.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        System.out.println(generation);
     }
     /*
     parameterize initGrid, checkNeighbors
--- src/ctisgolproject/GOLDrawer.java
+++ src/ctisgolproject/GOLDrawer.java
@@ -2,10 +2,15 @@ package ctisgolproject;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 
 public class GOLDrawer extends JFrame{
     //int cT = callThis;
+    Block theBlock = new Block();
+    int generation = 0;
     public GOLDrawer() {
         //0 is userInput, 1 is ex.1, 2 is ex.2, 3 is ex.3
         super("Square");
@@ -32,14 +37,31 @@ public class GOLDrawer extends JFrame{
             }
         }
     }
-
+    public void draw1(Graphics g, int x, int y, int gridSize, int n){
+        ArrayList<ArrayList<Block>> theGrid = theBlock.initGrid(n); //grid setup
+        for (int i=0;i<20;i++){//20 generations, parameterize this later
+            
+            theBlock.updateNeighborsAlive(theGrid, n);
+            theBlock.updateAlive(theGrid,n);
+            generation+=1;
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(CTISGOLPROJECT.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        System.out.println(generation);
+    }
     
     @Override
     public void paint(Graphics g) {
         System.out.println(UserInput.uInput);
         System.out.println(GOLexamples.cT);
         super.paint(g);
-        drawGrid(g,500,100,600,50);
+        if (GOLexamples.cT==0){
+            draw1(g,500,100,600,UserInput.gridLen);
+        }
+        //drawGrid(g,500,100,600,50);
         
     }
 }
